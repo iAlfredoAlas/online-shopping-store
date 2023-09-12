e3ca6db9b11cde500c40b7629a11dcd13d1af9fb
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-12 15:10:40 -0600

    Adds user entity

---
+++ src/main/java/com/kodigo/shopping/online/store/models/user.java
@@ -0,0 +1,51 @@
+package com.kodigo.shopping.online.store.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usuario")
+@SQLDelete(sql = "UPDATE user SET user_status = false WHERE id_user=?")
+@AllArgsConstructor
+@NoArgsConstructor
+public class user implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_user")
+    @Getter
+    @Setter
+    private Long idUser;
+
+    @Basic(optional = false)
+    @Column(name = "user_name")
+    @Getter
+    @Setter
+    private String userName;
+
+    @Basic(optional = false)
+    @Column(name = "password")
+    @Getter
+    @Setter
+    private String password;
+
+    @Basic(optional = false)
+    @Column(name = "user_mail")
+    @Getter
+    @Setter
+    private String userMail;
+
+    @Basic(optional = false)
+    @Column(name = "user_status")
+    @Getter
+    @Setter
+    private Boolean isUserActive;
+
+}
