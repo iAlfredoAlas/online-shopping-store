ba0ec893bb33a00982a18f27c5d6efdd1afc2c1c
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-13 15:41:49 -0600

    Adds RatingDTO entity

---
+++ src/main/java/com/kodigo/shopping/online/store/models/dto/RatingDTO.java
@@ -0,0 +1,47 @@
+package com.kodigo.shopping.online.store.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kodigo.shopping.online.store.models.Product;
+import com.kodigo.shopping.online.store.models.User;
+import com.kodigo.shopping.online.store.util.ETypeRating;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+
+public class RatingDTO {
+
+    @Getter
+    @Setter
+    private Long idRating;
+
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z]{0,39}+$", message = "The name must not contain spaces at the beginning, nor be longer than 40 characters.")
+    @NotBlank(message = "The field cannot remain empty.")
+    @Getter
+    @Setter
+    private String ratingDetail;
+
+    @Enumerated(EnumType.STRING)
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z]{0,8}+$", message = "The name must not contain spaces at the beginning, nor be longer than 9 characters.")
+    @NotBlank(message = "The field cannot remain empty.")
+    @Getter
+    @Setter
+    private ETypeRating typeRating;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "UTC")
+    @Getter
+    @Setter
+    private LocalDate dateRating;
+
+    @Getter
+    @Setter
+    private UserDTO idUser;
+
+    @Getter
+    @Setter
+    private ProductDTO idProduct;
+
+}
