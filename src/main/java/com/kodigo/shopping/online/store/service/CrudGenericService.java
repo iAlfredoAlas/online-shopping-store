58e2dbd7f8f146fc2e06c5e93b6326efeada4fc2
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-18 12:56:56 -0600

    Adds CrudGenericService

---
+++ src/main/java/com/kodigo/shopping/online/store/service/CrudGenericService.java
@@ -0,0 +1,19 @@
+package com.kodigo.shopping.online.store.service;
+
+import java.util.List;
+
+public interface CrudGenericService <T, ID> {
+
+    public List<T> getAll();
+
+    public List<T> findCustom(Boolean flat);
+
+    public T findById(Long id);
+
+    public T add(T model);
+
+    public T update(T model, Long id);
+
+    public void deleteLog(Long id);
+
+}
