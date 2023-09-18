584a92cea9065cee2894b469335dd12f62fcf3ab
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-18 12:51:24 -0600

    Adds IOrderDetailRepository

---
+++ src/main/java/com/kodigo/shopping/online/store/repository/IOrderDetailRepository.java
@@ -0,0 +1,6 @@
+package com.kodigo.shopping.online.store.repository;
+
+import com.kodigo.shopping.online.store.models.OrderDetail;
+
+public interface IOrderDetailRepository extends IGenericRepository<OrderDetail, Long> {
+}
