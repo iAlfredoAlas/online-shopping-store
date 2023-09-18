1f623353c0dc9b757fe06c0f82fabc970d1c1c9d
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-18 12:51:49 -0600

    Adds IRatingRepository

---
+++ src/main/java/com/kodigo/shopping/online/store/repository/IRatingRepository.java
@@ -0,0 +1,6 @@
+package com.kodigo.shopping.online.store.repository;
+
+import com.kodigo.shopping.online.store.models.Rating;
+
+public interface IRatingRepository extends IGenericRepository<Rating, Long>{
+}
