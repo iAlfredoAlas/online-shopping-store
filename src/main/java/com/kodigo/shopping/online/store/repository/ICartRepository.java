81ef71e2cf74e62104ddd86f9b3e6a8961f2fcd8
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-18 12:35:28 -0600

    Adds ICartRepository

---
+++ src/main/java/com/kodigo/shopping/online/store/repository/ICartRepository.java
@@ -0,0 +1,6 @@
+package com.kodigo.shopping.online.store.repository;
+
+import com.kodigo.shopping.online.store.models.Cart;
+
+public interface ICartRepository extends IGenericRepository<Cart, Long>{
+}
