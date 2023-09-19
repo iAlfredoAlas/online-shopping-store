1ab671ff1f8596713ed0da5bc7dd742c8b04abb8
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-19 11:24:39 -0600

    Add IRatingService

---
+++ src/main/java/com/kodigo/shopping/online/store/service/IRatingService.java
@@ -0,0 +1,6 @@
+package com.kodigo.shopping.online.store.service;
+
+import com.kodigo.shopping.online.store.models.Rating;
+
+public interface IRatingService extends CrudGenericService<Rating, Long>{
+}
