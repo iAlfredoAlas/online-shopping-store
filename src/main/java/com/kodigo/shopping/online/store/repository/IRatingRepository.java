d60de1c444f9894c12383dd2f34d2ccbcc1b94fb
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-19 11:24:22 -0600

    Add boolean method

--- src/main/java/com/kodigo/shopping/online/store/repository/IRatingRepository.java
+++ src/main/java/com/kodigo/shopping/online/store/repository/IRatingRepository.java
@@ -2,5 +2,10 @@ package com.kodigo.shopping.online.store.repository;
 
 import com.kodigo.shopping.online.store.models.Rating;
 
-public interface IRatingRepository extends IGenericRepository<Rating, Long>{
+import java.util.List;
+
+public interface IRatingRepository extends IGenericRepository<Rating, Long> {
+
+    List<Rating> findByIsRatingActive(boolean isRatingActive);
+
 }
