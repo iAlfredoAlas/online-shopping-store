0f7161bd16d9d845f7a3a7507f4f91c06f6fe343
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-19 10:57:21 -0600

    Add boolean method

--- src/main/java/com/kodigo/shopping/online/store/repository/IOrderDetailRepository.java
+++ src/main/java/com/kodigo/shopping/online/store/repository/IOrderDetailRepository.java
@@ -2,5 +2,10 @@ package com.kodigo.shopping.online.store.repository;
 
 import com.kodigo.shopping.online.store.models.OrderDetail;
 
+import java.util.List;
+
 public interface IOrderDetailRepository extends IGenericRepository<OrderDetail, Long> {
+
+    List<OrderDetail> findByIsOrderDetailActive(Boolean isOrderDetailActive);
+
 }
