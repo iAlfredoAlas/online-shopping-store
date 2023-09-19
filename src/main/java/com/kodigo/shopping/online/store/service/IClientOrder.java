7e0f1ac9f8a75f82299fe496d407aaf97b2f734f
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-19 09:09:24 -0600

    Adds IClientOrder

---
+++ src/main/java/com/kodigo/shopping/online/store/service/IClientOrder.java
@@ -0,0 +1,6 @@
+package com.kodigo.shopping.online.store.service;
+
+import com.kodigo.shopping.online.store.models.ClientOrder;
+
+public interface IClientOrder extends CrudGenericService<ClientOrder, Long> {
+}
