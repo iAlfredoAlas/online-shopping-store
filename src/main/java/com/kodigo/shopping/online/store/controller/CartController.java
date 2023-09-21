5a7046b9a31b91eaead112a3116e0309a9c561ac
Alfredo Alas <alfredo.alas14@gmail.com>
2023-09-21 03:14:12 -0600

    Add new method

--- src/main/java/com/kodigo/shopping/online/store/controller/CartController.java
+++ src/main/java/com/kodigo/shopping/online/store/controller/CartController.java
@@ -1,12 +1,14 @@
 package com.kodigo.shopping.online.store.controller;
 
 import com.kodigo.shopping.online.store.models.Cart;
+import com.kodigo.shopping.online.store.models.ClientOrder;
 import com.kodigo.shopping.online.store.service.ICartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,13 @@ public class CartController {
 
     @Autowired
     private ICartService cartService;
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<Cart>> getPage(@PageableDefault(size = 10) Pageable pageable){
+        log.info("Show all data");
+        Page<Cart> ccartPage = cartService.getAll(pageable);
+        return ResponseEntity.ok(ccartPage);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(Long id) {
